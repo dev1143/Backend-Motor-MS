76f4e22d99f4404969fa7638fc94c10aed0df604
debanjan <sinha.debanjan708@gmail.com>
2025-07-01 18:24:07 +0530

    added env db config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,9 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "pass@123",
-    DB: "ms_motor",
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
+    port: process.env.PORT || 3306,
     dialect: "mysql",
     pool: {
         max: 5,
